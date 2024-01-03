4dfcdfa511aac933c677051b522368a5fa7e4328
Mike Mulchrone <mtmulch0191@outlook.com>
2024-01-02 20:19:01 -0500

    #10 creating benchmark logging route

---
+++ API/Controllers/BenchmarkSDKMethodController.cs
@@ -0,0 +1,30 @@
+﻿using API.ControllerLogic;
+using CASHelpers;
+using CASHelpers.Types.HttpResponses.BenchmarkAPI;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
+
+namespace API.Controllers
+{
+    public class BenchmarkSDKMethodController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IBenchmarkSDKMethodControllerLogic _benchmarkSDKMethodControllerLogic;
+        public BenchmarkSDKMethodController(
+            IHttpContextAccessor contextAccessor,
+            IBenchmarkSDKMethodControllerLogic benchmarkSDKMethodControllerLogic
+            ) 
+        { 
+            this._contextAccessor = contextAccessor;
+            this._benchmarkSDKMethodControllerLogic = benchmarkSDKMethodControllerLogic;
+        }
+
+
+        [HttpPost]
+        [Route(Constants.ApiRoutes.MethodBenchmark)]
+        public async Task<IActionResult> CreateMethodSDKBenchmark([FromBody] BenchmarkSDKMethod sdkMethod)
+        {
+            return await this._benchmarkSDKMethodControllerLogic.CreateMethodSDKBenchmark(sdkMethod, this._contextAccessor.HttpContext);
+        }
+    }
+}
---
+++ Libraries/DataLayer/Mongo/Repositories/BenchmarkSDKMethodRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Mongo.Entities;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace DataLayer.Mongo.Repositories
+{
+    public class BenchmarkSDKMethodRepository : IBenchmarkSDKMethodRepository
+    {
+        private readonly IMongoCollection<BenchmarkSDKMethod> _benchmarkSDKMethods;
+
+        public BenchmarkSDKMethodRepository(IDatabaseSettings databaseSettings, IMongoClient client)
+        {
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            this._benchmarkSDKMethods = database.GetCollection<BenchmarkSDKMethod>("BenchmarkSDKMethods");
+        }
+
+        public async Task InsertSDKMethodBenchmark(BenchmarkSDKMethod method)
+        {
+            await this._benchmarkSDKMethods.InsertOneAsync(method);
+        }
+    }
+}
