07cbfc65c7a74bad137d2ada2bb7e254961bc0b4
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-17 20:30:14 -0400

    null checks in authroization middleware

--- Libraries/Validation/Attributes/ValidateJWTAttribute.cs
+++ Libraries/Validation/Attributes/ValidateJWTAttribute.cs
@@ -40,8 +40,8 @@ namespace Validation.Attributes
                 }
                 else
                 {
-                    string userId = readToken.Claims.First(x => x.Type == Constants.TokenClaims.Id).Value;
-                    string isAdmin = readToken.Claims.First(x => x.Type == Constants.TokenClaims.IsAdmin).Value;
+                    string userId = readToken.Claims.FirstOrDefault(x => x.Type == Constants.TokenClaims.Id).Value;
+                    string isAdmin = readToken.Claims.FirstOrDefault(x => x.Type == Constants.TokenClaims.IsAdmin).Value;
                     string subscriptionProductId = readToken.Claims.FirstOrDefault(x => x.Type == Constants.TokenClaims.SubscriptionPublicKey)?.Value;
                     context.HttpContext.Items[Constants.HttpItems.UserID] = userId;
                     context.HttpContext.Items[Constants.TokenClaims.IsAdmin] = isAdmin;
