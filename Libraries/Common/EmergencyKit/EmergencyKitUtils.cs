5d3127354ce2d8962e8ece3ce5f49478816f5a8c
Mike Mulchrone <mikemulchrone987@gmail.com>
2024-11-08 22:02:14 -0500

    #54 increasing cas version for hpke

---
+++ Libraries/Common/EmergencyKit/EmergencyKitUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common.EmergencyKit
+{
+    public static class EmergencyKitUtils
+    {
+        public static void CreateEmergencyKit()
+        {
+            string newEmergencyKitId = Guid.NewGuid().ToString();
+        }
+    }
+}
